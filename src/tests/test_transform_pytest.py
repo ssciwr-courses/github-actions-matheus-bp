29f4199cfe899f81ef9bc52c7cd5040685233bc3
matheus-bp <math.bp.sci@proton.me>
2025-06-27 10:09:23 +0200

    keep struggling with formatting again

--- src/tests/test_transform_pytest.py
+++ src/tests/test_transform_pytest.py
@@ -3,8 +3,10 @@ import transform as tf
 import pytest
 
 
-@pytest.mark.parametrize("myinput, myref", 
-[(1, np.pi), (0, 0), (2.1, np.pi * 2.1**2)])
+jls_extract_var = [(1, np.pi), (0, 0), (2.1, np.pi * 2.1**2)]
+
+
+@pytest.mark.parametrize("myinput, myref", jls_extract_var)
 def test_area_circ(myinput, myref):
     assert tf.area_circ(myinput) == myref
 
